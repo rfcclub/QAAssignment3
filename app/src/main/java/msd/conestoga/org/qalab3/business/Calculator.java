10c4789b7f4c22fdbb66d8e098fa70e7135701f7
Nam Tu <rfcclub2011@gmail.com>
2018-11-19 16:17:41 -0500

    Refactor naming

--- app/src/main/java/msd/conestoga/org/qalab3/business/Calculator.java
+++ app/src/main/java/msd/conestoga/org/qalab3/business/Calculator.java
@@ -6,19 +6,19 @@ public class Calculator {
 
     }
 
-    public int multiply(int lhs, int rhs) {
+    public int multiplication(int lhs, int rhs) {
         return rhs * lhs;
     }
 
-    public int subtract(int lhs, int rhs) {
+    public int subtraction(int lhs, int rhs) {
         return lhs - rhs;
     }
 
-    public int add(int lhs, int rhs) {
+    public int addition(int lhs, int rhs) {
         return lhs + rhs;
     }
 
-    public int divide(int lhs, int rhs) {
-        return lhs / rhs;
+    public int division(int lhs, int rhs) {
+        return lhs * rhs;
     }
 }
--- app/src/test/java/msd/conestoga/org/qalab3/CalculatorTest.java
+++ app/src/test/java/msd/conestoga/org/qalab3/CalculatorTest.java
@@ -10,120 +10,120 @@ import static org.junit.Assert.assertEquals;
 public class CalculatorTest {
 
     @Test
-    public void add_ThreeAndFour_ReturnSeven() {
+    public void addition_ThreeAndFour_ReturnSeven() {
         Calculator calculator = new Calculator();
         int lhs = 3, rhs = 4;
-        int result = calculator.add(lhs, rhs);
+        int result = calculator.addition(lhs, rhs);
         assertEquals(7, result);
     }
     @Test
-    public void add_ThreeAndNegativeFour_ReturnNegativeOne() {
+    public void addition_ThreeAndNegativeFour_ReturnNegativeOne() {
         Calculator calculator = new Calculator();
         int lhs = 3, rhs = -4;
-        int result = calculator.add(lhs, rhs);
+        int result = calculator.addition(lhs, rhs);
         assertEquals(-1, result);
     }
     @Test
-    public void add_NegativeThreeAndNegativeFour_ReturnNegativeSeven() {
+    public void addition_NegativeThreeAndNegativeFour_ReturnNegativeSeven() {
         Calculator calculator = new Calculator();
         int lhs = -3, rhs = -4;
-        int result = calculator.add(lhs, rhs);
+        int result = calculator.addition(lhs, rhs);
         assertEquals(-7, result);
     }
 
     @Test
-    public void subtract_FifteenAndTen_ReturnFive() {
+    public void subtraction_FifteenAndTen_ReturnFive() {
         Calculator calculator = new Calculator();
         int lhs = 15, rhs = 10;
-        int result = calculator.subtract(lhs, rhs);
+        int result = calculator.subtraction(lhs, rhs);
         assertEquals(5, result);
     }
 
     @Test
-    public void subtract_NegativeFifteenAndNegativeTen_ReturnNegativeFive() {
+    public void subtraction_NegativeFifteenAndNegativeTen_ReturnNegativeFive() {
         Calculator calculator = new Calculator();
         int lhs = -15, rhs = -10;
-        int result = calculator.subtract(lhs, rhs);
+        int result = calculator.subtraction(lhs, rhs);
         assertEquals(-5, result);
     }
 
     @Test
-    public void subtract_NegativeFifteenAndNegativeTen_ReturnNegativeTwentyFive() {
+    public void subtraction_NegativeFifteenAndNegativeTen_ReturnNegativeTwentyFive() {
         Calculator calculator = new Calculator();
         int lhs = 15, rhs = -10;
-        int result = calculator.subtract(lhs, rhs);
+        int result = calculator.subtraction(lhs, rhs);
         assertEquals(25, result);
     }
 
     @Test
-    public void multiply_FifteenAndTwo_ReturnThirty() {
+    public void multiplication_FifteenAndTwo_ReturnThirty() {
         Calculator calculator = new Calculator();
         int lhs = 15, rhs = 2;
-        int result = calculator.multiply(lhs, rhs);
+        int result = calculator.multiplication(lhs, rhs);
         assertEquals(30, result);
     }
 
     @Test
-    public void multiply_NegativeFifteenAndTwo_ReturnNegativeThirty() {
+    public void multiplication_NegativeFifteenAndTwo_ReturnNegativeThirty() {
         Calculator calculator = new Calculator();
         int lhs = -15, rhs = 2;
-        int result = calculator.multiply(lhs, rhs);
+        int result = calculator.multiplication(lhs, rhs);
         assertEquals(-30, result);
     }
 
     @Test
-    public void multiply_FifteenAndZero_ReturnZero() {
+    public void multiplication_FifteenAndZero_ReturnZero() {
         Calculator calculator = new Calculator();
         int lhs = 15, rhs = 0;
-        int result = calculator.multiply(lhs, rhs);
+        int result = calculator.multiplication(lhs, rhs);
         assertEquals(0, result);
     }
 
     @Test
-    public void divide_SixteenAndTwo_ReturnEight() {
+    public void division_SixteenAndTwo_ReturnEight() {
         Calculator calculator = new Calculator();
         int lhs = 16, rhs = 2;
-        int result = calculator.divide(lhs, rhs);
+        int result = calculator.division(lhs, rhs);
         assertEquals(8, result);
     }
 
     @Test
-    public void divide_NegativeSixteenAndTwo_ReturnNegativeEight() {
+    public void division_NegativeSixteenAndTwo_ReturnNegativeEight() {
         Calculator calculator = new Calculator();
         int lhs = -16, rhs = 2;
-        int result = calculator.divide(lhs, rhs);
+        int result = calculator.division(lhs, rhs);
         assertEquals(-8, result);
     }
 
     @Test
-    public void divide_ZeroByFifteen_returnZero() {
+    public void division_ZeroByFifteen_returnZero() {
         Calculator calculator = new Calculator();
         int lhs = 0, rhs = 15;
-        int result = calculator.divide(lhs, rhs);
+        int result = calculator.division(lhs, rhs);
         assertEquals(0, result);
     }
 
     @Test
-    public void divide_OneByOne_ReturnOne() {
+    public void division_OneByOne_ReturnOne() {
         Calculator calculator = new Calculator();
         int lhs = 1, rhs = 1;
-        int result = calculator.divide(lhs, rhs);
+        int result = calculator.division(lhs, rhs);
         assertEquals(1, result);
     }
 
     @Test
-    public void divide_TwoByOne_ReturnTwo() {
+    public void division_TwoByOne_ReturnTwo() {
         Calculator calculator = new Calculator();
         int lhs = 2, rhs = 1;
-        int result = calculator.divide(lhs, rhs);
+        int result = calculator.division(lhs, rhs);
         assertEquals(2, result);
     }
 
     @Test
-    public void divide_NineByThree_ReturnThree() {
+    public void division_NineByThree_ReturnThree() {
         Calculator calculator = new Calculator();
         int lhs = 9, rhs = 3;
-        int result = calculator.divide(lhs, rhs);
+        int result = calculator.division(lhs, rhs);
         assertEquals(3, result);
     }
 }
